ebc5a357a8033be0bfa6d8cac8012ae2618fb82c
Jan ROGUSZKA <jroguszk@e3r9p12.42.fr>
2017-12-14 18:15:24 +0100

    fdsfds

---
+++ fdf.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jroguszk <jroguszk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/14 13:58:18 by jroguszk          #+#    #+#             */
+/*   Updated: 2017/12/14 17:54:53 by jroguszk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minilibx/mlx.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+//int     my_key_funct(int keycode, void *param)
+//{
+//	printf("key event %d\n", keycode);
+//	mlx_pixel_put(mlx, win, 300, 300, 0xFF00FF);
+//	return (0);
+
+typedef	struct s_env
+{
+	void	*mlx;
+	void	*win;
+} 				t_env;
+
+void  draw(void *mlx, void *win)
+{
+	int     x;
+	int     y;
+
+	y = 100;
+	while (y < 200)
+	{
+		x = 100;
+		while (x < 200)
+		{
+			mlx_pixel_put(mlx, win, x, y, 0xFF0000);
+			x++;
+		}
+		y++;
+	}
+}
+
+int  mouse_hook(int button, int x, int y, t_env *e)
+{
+	printf("mouse: %d (%d:%d)\n", button, x, y);
+	return (0);
+}
+
+int  expose_hook(t_env *e)
+{
+	draw(e->mlx, e->win);
+	return (0);
+}
+
+int  key_hook(int keycode, t_env *e)
+{
+	printf("key: %d\n", keycode);
+	if (keycode == 65307)
+		exit (0);
+	return (0);
+}   
+
+int  main(int argc, char **argv)
+{
+	t_env	e;
+
+	e.mlx = mlx_init();
+	e.win = mlx_new_window(e.mlx, 420, 420, "42");
+	mlx_key_hook(e.win, key_hook, &e);
+	mlx_expose_hook(e.win, expose_hook, &e);
+	mlx_mouse_hook(e.win, mouse_hook, &e);
+	mlx_loop(e.mlx);
+	return (0);
+}
+
